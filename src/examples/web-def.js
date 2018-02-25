ededf86ad8c0e1b8fc6b86f1b710786a96995318
Eldar Gabdullin <eldargab@gmail.com>
2018-02-26 00:12:09 +0300

    Save work

--- src/examples/web-def.js
+++ src/examples/web-def.js
@@ -4,7 +4,10 @@ app.def('user', (db, cookies) => db.getUser(cookies.user))
 
 app.def('cookies', (req) => parse(req.headers['Cookie']))
 
-app.def('db', () => open('mysql://...'))
+app.def('db', (connString) => open(connString)))
+
+app.def('connString', () => process.ENV.DB_URL)
+
 
 app.def('main', function(Response) {
   http.createServer(function(req, res)) {
